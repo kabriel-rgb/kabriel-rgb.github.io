086961ebade2fcbc2294eea78a40957cfebee043
Kabriel <kabriel5373@gmail.com>
2026-04-13 23:25:45 -0300

    Fix URL redirection logic in script.js

--- script.js
+++ script.js
@@ -7,7 +7,9 @@
     } else if (procurar === "benicio" || procurar === "Benicio 3D" || procurar === "Benicio animação" || procurar === "Benicio" || procurar === "3" || procurar === "terceiro") {
       window.location.href = "https://kabriel-rgb.github.io/Benicio-3D/";
     } else if (procurar === "recaptcha" || procurar === "reCAPATCHA" || procurar === "não sou um robo" || procurar === "Não sou um robo" || procurar === "Não sou um robo " || procurar === "recaptcha " || procurar === "reCAPTCHA " || procurar === "não sou um robo " || procurar === "Recaptcha" || procurar === "4") { 
-      window.location.href = ("https://kabriel-rgb.github.io/reCAPTCHA");
+      window.location.href = ("https://kabriel-rgb.github.io/reCAPTCHA";
+    } else if (procurar === "Monte um relógio Condor" || procurar === "Relógio" || procurar === "relogio" || procurar === "Relogio" || procurar === "Condor" || procurar === "quarto" || procurar === "4") {
+      window.location.href = ("https://kabriel-rgb.github.io/monterelogiocondor.github.io";
     } else {
       alert("opção não encontrada!")
     }
